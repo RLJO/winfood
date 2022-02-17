5d346142aec0f0fc8f2531de1804c424985f853e
odooerpumg <odooerpumg@gmail.com>
2022-02-17 10:18:16 +0630

    Approve button accessright of Purchase Order

--- addons/purchase_extension/models/purchase_order.py
+++ addons/purchase_extension/models/purchase_order.py
@@ -13,3 +13,21 @@ class PurchaseOrder(models.Model):
     def action_approve(self):
         for rec in self:
             rec.approve_state = 'approve'
+
+    def get_current_user(self):
+        result = False
+        user_id = self.env.uid
+        user = self.env['res.users'].browse(user_id)
+        is_purchase_user_id = False
+        group_purchase_user_id = self.env['ir.model.data'].get_object_reference('purchase', 'group_purchase_user')[1]
+
+        group_manager_id = self.env['ir.model.data'].get_object_reference('purchase', 'group_purchase_manager')[1]
+
+        if group_purchase_user_id and not group_manager_id in [g.id for g in user.groups_id]:
+            is_purchase_user_id = True
+
+        if is_purchase_user_id:
+            result=True
+        self.get_user = result
+
+    get_user =fields.Boolean(compute='get_current_user',string='Get User')
