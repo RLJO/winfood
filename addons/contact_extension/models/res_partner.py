218c95bd136dc43d14bf541a03a7791fbfc18ffc
odooerpumg <odooerpumg@gmail.com>
2022-02-24 14:38:37 +0630

    changing partner address format

--- addons/contact_extension/models/res_partner.py
+++ addons/contact_extension/models/res_partner.py
@@ -30,20 +30,41 @@ class ResPartner(models.Model):
             pass
         return result
 
-    # def write(self, vals):
-    #     phone = vals.get('phone')
-    #     record_id = self.env['res.partner'].browse(self.id)
-    #     user_ids = self.env['res.users'].search([('partner_id','=',self.id)])
-    #     result = super(ResPartner, self).write(vals)
-    #     if phone:
-    #         gene_password = randint(10 ** (6 - 1), (10 ** 6) - 1)
-    #         record_id.write({'gene_password': gene_password})
-    #         user_ids.write({
-    #             'name': self.name,
-    #             'login': phone,
-    #             'password': str(gene_password),
-    #         })
-    #     return result
+
+    def _display_address(self, without_company=False):
+
+        '''
+        The purpose of this function is to build and return an address formatted accordingly to the
+        standards of the country where it belongs.
+
+        :param address: browse record of the res.partner to format
+        :returns: the address formatted in a display that fit its country habits (or the default ones
+            if not country is specified)
+        :rtype: string
+        '''
+        # get the information that will be injected into the display format
+        # get the address format
+        address_format = self._get_address_format()
+        address_format = "%(street)s %(street2)s %(township_name)s %(city_id)s %(country_name)s"
+        
+        args = {
+            'state_code': self.state_id.code or '',
+            'state_name': self.state_id.name or '',
+            'city_id': self.city_id.name or '',
+            'township_name': self.township_name.name or '',
+            'country_code': self.country_id.code or '',
+            'country_name': self._get_country_name(),
+            'company_name': self.commercial_company_name or '',
+        }
+        for field in self._formatting_address_fields():
+            args[field] = getattr(self, field) or ''
+        if without_company:
+            args['company_name'] = ''
+        elif self.commercial_company_name:
+            address_format = '%(company_name)s\n' + address_format
+        result = address_format % args
+        return result
+
 
 class City(models.Model):
     _name = 'winfood.city'
