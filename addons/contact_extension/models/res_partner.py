13c2b9823e9126926fe099c5507b5c2567e1f0d5
odooerpumg <odooerpumg@gmail.com>
2022-02-24 11:52:33 +0630

    If new customers only create, can generate new password(no need for vendor, user, employee)

--- addons/contact_extension/models/res_partner.py
+++ addons/contact_extension/models/res_partner.py
@@ -1,5 +1,6 @@
-from odoo import fields, api, models
+from odoo import fields, api, models, _
 from random import randint
+from odoo.exceptions import UserError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -13,8 +14,9 @@ class ResPartner(models.Model):
     def create(self, vals):
         name = vals.get('name')
         phone = vals.get('phone')
+        # menu_id = self._context.get('params').get('menu_id') if self._context.get('params') else None
         result = super(ResPartner, self).create(vals)
-        if phone:
+        if self._context.get('default_customer_rank') and phone:
             gene_password = randint(10 ** (6 - 1), (10 ** 6) - 1)
             result.write({'gene_password': gene_password})
             self.env['res.users'].create({
@@ -23,23 +25,25 @@ class ResPartner(models.Model):
                 'password': str(gene_password),
                 'partner_id': result.id,
                 'groups_id': [(6, 0, [self.env.ref('base.group_portal').id])],
-            })
+            })                
+        else:
+            pass
         return result
 
-    def write(self, vals):
-        phone = vals.get('phone')
-        record_id = self.env['res.partner'].browse(self.id)
-        user_ids = self.env['res.users'].search([('partner_id','=',self.id)])
-        result = super(ResPartner, self).write(vals)
-        if phone:
-            gene_password = randint(10 ** (6 - 1), (10 ** 6) - 1)
-            record_id.write({'gene_password': gene_password})
-            user_ids.write({
-                'name': self.name,
-                'login': phone,
-                'password': str(gene_password),
-            })
-        return result
+    # def write(self, vals):
+    #     phone = vals.get('phone')
+    #     record_id = self.env['res.partner'].browse(self.id)
+    #     user_ids = self.env['res.users'].search([('partner_id','=',self.id)])
+    #     result = super(ResPartner, self).write(vals)
+    #     if phone:
+    #         gene_password = randint(10 ** (6 - 1), (10 ** 6) - 1)
+    #         record_id.write({'gene_password': gene_password})
+    #         user_ids.write({
+    #             'name': self.name,
+    #             'login': phone,
+    #             'password': str(gene_password),
+    #         })
+    #     return result
 
 class City(models.Model):
     _name = 'winfood.city'
